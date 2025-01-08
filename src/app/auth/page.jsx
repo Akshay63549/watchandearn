b7f49ecee588a0daa8f935cf2b65d7e3363de34a
Your Name <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>
2025-01-08 10:44:05 +0530

    initial setup

---
+++ src/app/auth/page.jsx
@@ -0,0 +1,12 @@
+import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
+import React from 'react'
+
+export default function page() {
+  return (
+   <>
+   <Login/>
+   <SignUp/>
+   </>
+  )
+}
